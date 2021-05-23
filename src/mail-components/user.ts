6b7dcdbb21c08bc8bbf8b79316d5f2c6be2e7df3
BeADia <koc0819@gmail.com>
2021-05-23 23:51:54 +0900

    fix userinfo save in storage

--- src/mail-components/user.ts
+++ src/mail-components/user.ts
@@ -1,20 +1,36 @@
-const userInfo = {
-  email: "98gudcks@gmail.com",
-  password: "ehlswkd1!!",
-  host: "imap.gmail.com",
-};
-let alarmList = ["seongsu1"];
 export const getAlarmList = () => {
+  const current = localStorage.getItem("mailInfo.alarmList");
+  let alarmList = [];
+  if (current) {
+    alarmList = JSON.parse(current);
+  }
   return alarmList;
 };
 export const setAlarmList = (value: string) => {
+  const current = localStorage.getItem("mailInfo.alarmList");
+  let alarmList = [];
+  if (current) {
+    alarmList = JSON.parse(current);
+  }
   alarmList.push(value);
+  localStorage.setItem("mailInfo.alarmList", JSON.stringify(alarmList));
+};
+export const removeAlarmList = () => {
+  localStorage.removeItem("mailInfo.alarmList");
 };
 export const getUserInfo = () => {
-  return userInfo;
+  const email = localStorage.getItem("mailInfo.email");
+  const password = localStorage.getItem("mailInfo.password");
+  const host = localStorage.getItem("mailInfo.host");
+  const result = {
+    email,
+    password,
+    host,
+  };
+  return result;
 };
 export const setUserInfo = (email: string, password: string, host: string) => {
-  userInfo.email = email;
-  userInfo.password = password;
-  userInfo.host = host;
+  localStorage.setItem("mailInfo.email", email);
+  localStorage.setItem("mailInfo.password", password);
+  localStorage.setItem("mailInfo.host", host);
 };

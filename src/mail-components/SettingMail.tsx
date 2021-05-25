b11c6a2d1d411879fcef286391614a476bfd8183
yunseo323 <jokani3244833@gmail.com>
2021-05-26 02:23:15 +0900

    Add email popup style

--- src/mail-components/SettingMail.tsx
+++ src/mail-components/SettingMail.tsx
@@ -1,5 +1,50 @@
 import React from "react";
 import * as user from "./user";
+import styled from "styled-components";
+
+const SettingStyle = styled.div`
+  margin: 1rem;
+  font-size: 20px;
+  .container {
+    display: flex;
+  }
+  .box {
+    margin-bottom: 1rem;
+  }
+  .topBlock {
+    display: flex;
+    flex-direction: column;
+  }
+  label {
+    color: #495057;
+  }
+  input {
+    width: 10rem;
+    border: none;
+    border-bottom: 1px solid black;
+    text-align: center;
+    outline: none;
+    font-size: 15px;
+  }
+  #domain{
+    width:20rem;
+  }
+  input[type="submit"] {
+    width: 4rem;
+    background:#6c63ff;
+    color:white;
+    font-size: 15px;
+    border: none;
+  }
+  button{
+    width: 4rem;
+    background:#6c63ff;
+    color:white;
+    font-size: 15px;
+    border: none;
+    margin-left: 3rem;
+  }
+`;
 
 const SettingMail = () => {
   const emailRef = React.useRef(null);
@@ -40,22 +85,34 @@ const SettingMail = () => {
   return (
     <div>
       <div>
+        <SettingStyle>
         <form
           onSubmit={(event) => {
             onSubmit(event);
-          }}
-        >
+          }}>
+            <div className="container">
+            <div className="topBlock">
+          <div className="box">
           <label>
             이메일 주소를 입력해주세요 : <input ref={emailRef} />
           </label>
+          </div>
+          <div className="box">
           <label>
             비밀번호를 입력해주세요 : <input ref={passwordRef} />
           </label>
-
+          </div>
+          
+          </div>
           <input type="submit" value="Submit" />
+            </div>
+          
         </form>
+        </SettingStyle>
       </div>
+      
       <div>
+      <SettingStyle>
         <form
           onSubmit={(event) => {
             event.preventDefault();
@@ -71,13 +128,13 @@ const SettingMail = () => {
         >
           <label>
             중요한 이메일 발신자 혹은 도메인을 입력해주세요 :{" "}
-            <input ref={alarmRef} />
+            <input id="domain" ref={alarmRef} />
           </label>
           <input type="submit" value="등록" />
           <ul>
             {alarmList
               ? alarmList.map((value) => (
-                  <li id={String(alarmList.indexOf(value))}>
+                  <div id={String(alarmList.indexOf(value))}>
                     {value}
                     <button
                       onClick={() =>
@@ -86,7 +143,7 @@ const SettingMail = () => {
                     >
                       삭제
                     </button>
-                  </li>
+                  </div>
                 ))
               : null}
           </ul>
@@ -99,7 +156,10 @@ const SettingMail = () => {
           >
             모두 삭제
           </button>
-        </form>
+          </form>
+          </SettingStyle>
+        
+        
       </div>
     </div>
   );

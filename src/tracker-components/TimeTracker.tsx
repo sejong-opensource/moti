6fc09ed3aee433f60502b8bc7efde9745db5d409
yunseo323 <jokani3244833@gmail.com>
2021-05-30 12:40:06 +0900

    Add time-tracker-style

--- src/tracker-components/TimeTracker.tsx
+++ src/tracker-components/TimeTracker.tsx
@@ -2,8 +2,49 @@
 import React from 'react';
 import {useState,useEffect,useRef} from 'react';
 //import activeWindow from 'active-win';
+import styled from "styled-components";
 
-
+const Container = styled.div `
+display: flex;
+flex-direction: column;
+  .input-box{
+    margin-top: 1rem;
+    margin-left: 1rem;
+    display: flex;
+    height: 3rem;
+    input{
+      width: 12rem;
+      background: none;
+      border: none;
+      border-bottom: 1px solid rgba(255, 255, 255, 0.95);
+      text-align: center;
+      outline: none;
+      font-size: 15px;
+    }
+  }
+`
+const ButtonStyle = styled.div`
+  display: flex;
+  padding: 0.3rem;
+  button {
+    margin-left: 0.3rem;
+    margin-right: 0.3rem;
+    width: 3xrem;
+    height: 3rem;
+    border-radius: 0.3rem;
+    border: none;
+    background: transparent;
+    border: 1px solid rgba(255, 255, 255, 0.95);
+    color: rgba(255, 255, 255, 0.95);
+    cursor: pointer;
+    transition-property: background, color;
+    transition-duration: 0.3s;
+    &:hover {
+      background: rgba(255, 255, 255, 0.95);
+      color: black;
+    }
+  }
+`
 
 const UserUrl =()=>{
     const [list, setList] = useState([]);
@@ -59,17 +100,28 @@ const UserUrl =()=>{
   }
       return (
         <div>
-          <input placeholder="ex) *://*.facebook.com/*" value={userUrl} onChange={onChange}  />
-        <button onClick={onInsert}>add</button>
-        <ul>
-            {/* {localArr&&localArr.map((value, nextId) => (
-              <li key={nextId} >url : {value}</li>
-            ))} */}
-             {list && list.map((value, nextId) => (
-              <li key={nextId} >url : {value}</li>
-            ))}
-        </ul>
-        <button onClick={clear}>clear</button>
+          <Container>
+            <div className="input-box">
+              <input placeholder="ex) *://*.facebook.com/*" value={userUrl} onChange={onChange}  />
+              <ButtonStyle>
+              <button onClick={onInsert}>add</button>
+              </ButtonStyle>
+            </div>
+          
+          
+            <ul>
+                {/* {localArr&&localArr.map((value, nextId) => (
+                  <li key={nextId} >url : {value}</li>
+                ))} */}
+                {list && list.map((value, nextId) => (
+                  <li key={nextId} >url : {value}</li>
+                ))}
+            </ul>
+            <ButtonStyle>
+            <button onClick={clear}>clearAll</button>
+            </ButtonStyle>
+          </Container>
+          
         </div>
       );
 };

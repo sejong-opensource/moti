2f8e87e2c8ae6d9bc6daecd03c5e751658a4247e
yunseo323 <jokani3244833@gmail.com>
2021-05-30 20:11:16 +0900

    Add PhraseTemplate

---
+++ src/phrase-components/PhraseTemplate.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const PhraseTemplateBlock = styled.div `
+  flex:1;
+  height: 50vh;
+  position: relative;
+  background: rgb(32,28,53);
+  border-radius: 16px;
+  //box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.04);
+
+  margin: 0 auto; 
+  display: flex;
+  flex-direction: column;
+`;
+
+function PhraseTemplate({children}) {
+    return <PhraseTemplateBlock>{children}</PhraseTemplateBlock>;
+}
+
+export default PhraseTemplate;
